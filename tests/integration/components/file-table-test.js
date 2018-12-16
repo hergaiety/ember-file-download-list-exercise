7229af4fdb45e132c04288f3b8aaf0ce18b4666c
Joe Wroten <joe@wroten.me>
2018-12-16 13:33:21 -0600

    Test covering file-table loop for file-table-item's

--- tests/integration/components/file-table-test.js
+++ tests/integration/components/file-table-test.js
@@ -1,7 +1,16 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, find } from '@ember/test-helpers';
+import { render, find, findAll } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+
+const genericFile = () => {
+  return {
+    name: 'foo.bar',
+    device: 'Baz',
+    path: '~/foo.bar',
+    status: 'available',
+  };
+};
 
 module('Integration | Component | file-table', function(hooks) {
   setupRenderingTest(hooks);
@@ -15,5 +24,12 @@ module('Integration | Component | file-table', function(hooks) {
     assert.ok(find('[data-test-action=file-table-download-selected]'), 'Download selected');
     assert.ok(find('[data-test-id=file-table-header]'), 'Table header');
     assert.notOk(find('[data-test-id=file-table-item]'), 'Should have no table rows without data');
+  });
+
+  test('it renders rows of files', async function(assert) {
+    this.set('files', [genericFile(), genericFile()]);
+    await render(hbs`{{file-table files=files}}`);
+
+    assert.equal(findAll('[data-test-id=file-table-item]').length, 2, 'Found expected two rows');
   });
 });
